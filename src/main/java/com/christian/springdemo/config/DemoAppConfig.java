0b874fed384363d70e2c57e81589cebb1ad89215
Christian <christian@sanglech.com>
2021-04-23 13:58:29 +0900

    Initial Commit for HelloWorld Spring rest

---
+++ src/main/java/com/christian/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.christian.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.christian.springdemo")
+public class DemoAppConfig {
+
+}
